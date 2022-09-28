9756ba2c79713ae0d4636dbaa548a2034f908535
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-28 13:06:14 +0700

    add fuction and styling

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,201 @@
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => {
-				search_results.value = res.results;
+				search_results.value = res.data;
 			});
 	};
+
+	const handleInput = (e) => {
+		if (!e.target.value) {
+			search_results.value = [];
+		}
+	};
+
+	const addAnime = (anime) => {
+		search_results.value = [];
+		query.value = "";
+
+		my_anime.value.push({
+			id: anime.mal_id,
+			title: anime.title,
+			image: anime.image.jpg.image_url,
+			total_episodes: anime.episodes,
+			watched_episodes: 0,
+		});
+
+		localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+	};
+
+	const increaseWatch = (anime) => {
+		anime.watched_episodes++;
+		localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+	};
+	const decreaseWatch = (anime) => {
+		anime.watched_episodes--;
+		localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+	};
+
+	onMounted(() => {
+		my_anime.value = JSON.parse(localStorage.getItem("my_anime")) || [];
+	});
 </script>
 
 <template>
-	<main>Hello World</main>
+	<main>
+		<h1>My Anime List</h1>
+		<form @submit.prevent="searchAnime">
+			<input type="text" placeholder="Search for an anime" v-model="query" @input="handleInput" />
+			<button type="submit">Search</button>
+		</form>
+
+		<div class="results" v-if="search_results.length > 0">
+			<div class="result" v-for="anime in search_results">
+				<img :src="anime.images.jpg.image_url" alt="picture" />
+				<div class="details">
+					<h3>{{ anime.title }}</h3>
+					<p :title="anime.synopsis" v-if="anime.synopsis">{{ anime.synopsis.slice(0, 120) }} ...</p>
+					<span class="flex-1"></span>
+					<button @click="addAnime(anime)">Add to my anime</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="myanime" v-if="my_anime.length > 0">
+			<h2>My Anime</h2>
+
+			<div v-for="anime in my_anime_asc" class="anime">
+				<img :src="anime.image" />
+				<h3>{{ anime.title }}</h3>
+				<div class="flex-1"></div>
+				<span class="episodes"> {{ anime.watched_episodes }} / {{ anime.total_episodes }} </span>
+				<button v-if="anime.watched_episodes !== anime.total_episodes" @click="increaseWatch(anime)">+</button>
+				<button v-if="anime.watched_episodes > 0" @click="decreaseWatc(anime)">-</button>
+			</div>
+		</div>
+	</main>
 </template>
 
-<style></style>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	}
+
+	body {
+		background-color: whitesmoke;
+	}
+
+	main {
+		margin: 0 auto;
+		max-width: 768px;
+		padding: auto 1.5rem;
+	}
+
+	h1 {
+		text-align: center;
+		margin-bottom: 1.5rem;
+	}
+
+	form {
+		display: flex;
+		max-width: 480px;
+		margin: 0 auto 1.5rem;
+	}
+
+	form input {
+		appearance: none;
+		outline: none;
+		border: none;
+		background: white;
+
+		display: block;
+		color: cadetblue;
+		font-size: 1.125 rem;
+		padding: 0.5rem 1rem;
+		width: 100%;
+	}
+
+	.results {
+		background-color: white;
+		border-radius: 0.5 rem;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		max-height: 480px;
+		overflow-y: scroll;
+		margin-bottom: 1.5rem;
+	}
+
+	.result {
+		display: flex;
+		margin: 1rem;
+		padding: 1rem;
+		border: 1px solid #ccc;
+		border-radius: 0.5rem;
+		transition: 0.4s;
+	}
+
+	.result img {
+		width: 100px;
+		border-radius: 1rem;
+		margin-right: 1rem;
+	}
+
+	.details {
+		flex: 1 1 0%;
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+	}
+
+	.flex-1 {
+		flex: 1 1 0%;
+	}
+
+	.details h3 {
+		font-size: 1.25rem;
+		margin-bottom: 0.5rem;
+	}
+
+	.details p {
+		font-size: 0.875rem;
+		line-height: 1.4;
+		margin-bottom: 0.5rem;
+	}
+
+	.details .button {
+		margin-left: auto;
+	}
+
+	.mayanime .anime {
+		display: flex;
+		align-items: center;
+		margin-bottom: 1.5rem;
+		background-color: white;
+		padding: 1rem;
+		border-radius: 0.5rem;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	.anime img {
+		width: 72px;
+		height: 72px;
+		object-fit: cover;
+		border-radius: 1rem;
+		margin-right: 1rem;
+	}
+
+	.anime h3 {
+		color: burlywood;
+		font-size: 1.125rem;
+	}
+
+	.anime .episodes {
+		margin-right: 1rem;
+		color: burlywood;
+	}
+
+	.anime .button:first-of-type {
+		margin-right: 1rem;
+	}
+</style>
